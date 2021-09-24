e41b13a0702c8fd3a785a89007540695e0584f16
Carter <carter531@icloud.com>
2021-09-23 18:33:09 -0700

    final

---
+++ tree.js
@@ -0,0 +1,73 @@
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+  constructor(val, children = []) {
+    this.val = val;
+    this.children = children;
+  }
+}
+
+class Tree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** sumValues(): add up all of the values in the tree. */
+
+  sumValues() {
+    if (!this.root) return 0;
+    let total = this.root.val;
+    function sumHelper(node) {
+      for (let child of node.children) {
+        total += child.val;
+        if (child.children.length > 0) {
+          sumHelper(child);
+        }
+      }
+    }
+    sumHelper(this.root);
+    return total;
+  }
+
+  /** countEvens(): count all of the nodes in the tree with even values. */
+
+  countEvens() {
+    if (!this.root) return 0;
+    let total = this.root.val;
+    function evenCounter(node) {
+      for (let child of node.children) {
+        if (child % 2 == 0) total++;
+        if (child.children.length > 0) {
+          evenCounter(child);
+        }
+      }
+    }
+    evenCounter(this.root);
+    return total;
+  }
+
+  /** numGreater(lowerBound): return a count of the number of nodes
+   * whose value is greater than lowerBound. */
+
+  numGreater(lowerBound) {
+    if (!this.root) return 0;
+    let total = this.root.val > lowerBound ? 1 : 0;
+    function countEvensHelper(node) {
+      // go through all the children for a Node
+      for (let child of node.children) {
+        // count the child if the value is greater than lowerBound
+        if (child.val > lowerBound) count++;
+        // if it has any children
+        if (child.children.length > 0) {
+          // recurse with the child as the root
+          countEvensHelper(child);
+        }
+      }
+    }
+
+    countEvensHelper(this.root);
+    return total;
+  }
+}
+
+module.exports = { Tree, TreeNode };
